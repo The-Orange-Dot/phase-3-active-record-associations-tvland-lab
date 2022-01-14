65e9141c39b84b0aa0d0798748b453ae890df026
TheOrangeDot <theorangedottwitch@gmail.com>
2022-01-14 22:52:15 +0900

    Completed

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,20 @@
 class Actor < ActiveRecord::Base
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    "#{self.first_name} #{self.last_name}"
+  end
+
+  def list_roles
+    i = 0
+    list = []
+    while i < self.characters.length do 
+      list << "#{self.characters[i].name} - #{self.shows[i].name}"
+      i += 1 
+    end
+    list
+  end
+
   
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,15 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    i = 0
+    list = []
+    while i < self.actors.length do 
+      list << "#{self.actors[i].first_name} #{self.actors[i].last_name}"
+      i += 1 
+    end
+    list
+  end
 end
